98ed2a79e05d852b3edd828a6867802f4735dbfd
David M. Chen <chen7david@me.com>
2022-12-05 14:45:45 +0800

    updated basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,83 @@
-import { Avatar, Button, Card, Col, Layout, Menu, Row, Space } from 'antd';
-import React, { useState } from 'react';
-import Icon, { HomeOutlined, UserOutlined, ShopOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Avatar, Button, Card, Col, Layout, Menu, Row, Space } from 'antd'
+import React, { useState } from 'react'
+import Icon, {
+  HomeOutlined,
+  UserOutlined,
+  ShopOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons'
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Sider, Content } = Layout
 
 const menuItems = [
   {
     key: '1',
     label: 'Home',
-    icon: <HomeOutlined />
+    icon: <HomeOutlined />,
   },
   {
     key: '2',
     label: 'Profile',
-    icon: <UserOutlined />
+    icon: <UserOutlined />,
   },
   {
     key: '3',
     label: 'Store',
-    icon: <ShopOutlined />
+    icon: <ShopOutlined />,
   },
 ]
 
 function App() {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <Layout style={{height: '100vh'}}>
-      <Sider trigger={null} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-      <div className="logo" />
-          <Menu mode="inline" theme="dark" items={menuItems}></Menu>
+    <Layout style={{ height: '100vh' }}>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="logo" />
+        <Menu mode="inline" theme="dark" items={menuItems}></Menu>
       </Sider>
-      
+
       <Layout>
-      <Header>
-        <Icon type='menu' style={{ fontSize: '16px', color: '#FFF' }}/>
-        <MenuUnfoldOutlined onClick ={() => setCollapsed(!collapsed)} style={{ fontSize: '16px', color: '#FFF' }}/>
-      </Header>
-        <Content style={{padding: 10}}>
+        <Header>
+          <Icon type="menu" style={{ fontSize: '16px', color: '#FFF' }} />
+          <MenuUnfoldOutlined
+            onClick={() => setCollapsed(!collapsed)}
+            style={{ fontSize: '16px', color: '#FFF' }}
+          />
+        </Header>
+        <Content style={{ padding: 10 }}>
           main content
           <Row>
             <Col>
-            <Space>
-              <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-              <Card size="small" title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-            </Card>
-            </Space>
+              <Space>
+                <Card
+                  title="Default size card"
+                  extra={<a href="#">More</a>}
+                  style={{ width: 300 }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+                <Card
+                  size="small"
+                  title="Small size card"
+                  extra={<a href="#">More</a>}
+                  style={{ width: 300 }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </Space>
             </Col>
           </Row>
-          
         </Content>
       </Layout>
     </Layout>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'antd/dist/antd.css'
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
